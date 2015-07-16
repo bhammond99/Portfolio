032ba3550bcb202b14669c1140fd6202b2418073
brycehammond <hammond.bryce99@gmail.com>
2015-07-16 15:47:38 -0400

    added paperclip

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -20,6 +20,7 @@ class LinksController < ApplicationController
 
   # GET /links/1/edit
   def edit
+    @link = Link.find(params[:id])
   end
 
   # POST /links
@@ -70,6 +71,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:title, :url)
+      params.require(:link).permit(:title, :url, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715194712) do
+ActiveRecord::Schema.define(version: 20150716165124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20150715194712) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
